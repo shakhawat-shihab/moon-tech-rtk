1076a8dede266848ed2c265b425642933ff4e7e8
SHAKHAWAT <shihabchtg@gmail.com>
2023-01-02 11:39:12 +0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import { Provider } from "react-redux";
+import { RouterProvider } from "react-router-dom";
+import store from "./app/store";
+import routes from "./routes/routes";
+
+function App() {
+  return (
+    <div>
+      <Provider store={store}>
+        <RouterProvider router={routes} />
+      </Provider>
+
+    </div>
+  );
+}
+
+export default App;
